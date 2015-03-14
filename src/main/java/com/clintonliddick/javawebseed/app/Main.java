24d888bb2d2917b06bf7a2791ab25ead8ee76d1f
Clint Liddick <clint@clintonliddick.com>
2015-03-13 23:57:45 -0400

    basic INI file auth with Shiro

--- src/main/java/com/clintonliddick/javawebseed/app/Main.java
+++ src/main/java/com/clintonliddick/javawebseed/app/Main.java
@@ -1,6 +1,10 @@
 package com.clintonliddick.javawebseed.app;
 
 import com.clintonliddick.javawebseed.rest.Hello;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.util.Factory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -12,11 +16,16 @@ import org.slf4j.LoggerFactory;
  * Server start-up class
  */
 public final class Main {
-    public static void main(String[] args) throws Exception {
-        final Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.debug("debug logging enabled");
-        logger.info("info logging enabled");
+    private static final transient Logger log = LoggerFactory.getLogger(Main.class);
 
+    public static void main(String[] args) throws Exception {
+        // Setup Shiro security
+        Factory<SecurityManager> securityManagerFactory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = securityManagerFactory.getInstance();
+        // TODO instead of singleton, make this part of the container configuration (web.xml)
+        SecurityUtils.setSecurityManager(securityManager);
+
+        // Setup Jetty/Jersey server
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
         context.setContextPath("/rest");
 
--- src/main/java/com/clintonliddick/javawebseed/rest/Hello.java
+++ src/main/java/com/clintonliddick/javawebseed/rest/Hello.java
@@ -1,5 +1,12 @@
 package com.clintonliddick.javawebseed.rest;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,14 +20,45 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("/hello")
 public class Hello {
-    final Logger logger = LoggerFactory.getLogger(Hello.class);
+    final Logger log = LoggerFactory.getLogger(Hello.class);
 
     @GET
     @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        logger.info("returning \"Hello world!\"");
-        return "Hello world!";
+//        AuthenticationToken authToken = new AuthenticationToken() {
+//            @Override
+//            public Object getPrincipal() {
+//                return null;
+//            }
+//
+//            @Override
+//            public Object getCredentials() {
+//                return null;
+//            }
+//        };
+        UsernamePasswordToken token = new UsernamePasswordToken("presidentskroob", "12345");
+        Subject user = SecurityUtils.getSubject();
+//        if (!user.isAuthenticated()) { // unneccessary for stateless/sessionless endpoint
+        try {
+            user.login(token);
+        } catch (IncorrectCredentialsException ice) {
+            //password didn't match, try again?
+        } catch (LockedAccountException lae) {
+            //account for that username is locked - can't login.  Show them a message?
+        } catch (AuthenticationException ae) {
+            //unexpected condition - error?
+        }
+
+        log.info("/hello called by user: {}", user.getPrincipal());
+
+        if (user.hasRole("guest")) {
+            log.info("returning \"Hello world!\"");
+            user.logout();
+            return "Hello world!";
+        } else {
+            throw new UnauthorizedException("User unauthorized to call /hello");
+        }
     }
 
 }
